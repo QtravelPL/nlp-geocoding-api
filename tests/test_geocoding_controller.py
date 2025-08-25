e57b58fde147084a9bab99b85fd270cc78feef87
anton-0 <anton.delinac@gmail.com>
2025-08-25 11:15:20 +0200

    cache tests

--- tests/test_geocoding_controller.py
+++ tests/test_geocoding_controller.py
@@ -1,6 +1,7 @@
 import json
-from unittest.mock import MagicMock, patch
+from unittest.mock import patch
 
+import httpx
 import pytest
 
 from src.geocoding import GeocodingController
@@ -13,41 +14,83 @@ with open("fixtures/london_response.json", "r") as response, open(
 
 
 @pytest.mark.asyncio
+@patch("src.geocoding.redis_client.get")
+@patch("src.geocoding.redis_client.set")
 @patch(
     "src.geocoding.load_url_and_api_key",
     return_value=("http://mock-url", "mock-api-key"),
 )
 @patch("httpx.AsyncClient.get")
-async def test_get_coordinates_success(mock_http_get, mock_load_url_and_api_key):
-    mock_response = MagicMock()
-    mock_response.raise_for_status.return_value = None
-    mock_response.json.return_value = london_fixture
+async def test_get_coordinates_google_api_hit(
+    mock_http_get, mock_load_url_and_api_key, mock_redis_set, mock_redis_get
+):
+    mock_response = httpx.Response(
+        status_code=200,
+        content=json.dumps(london_fixture),
+        request=httpx.Request("GET", "http://mock-url"),
+    )
     mock_http_get.return_value = mock_response
+    mock_redis_get.return_value = None
 
     controller = GeocodingController()
     result = await controller.get_coordinates([["london"]])
 
-    assert result == london_expected
     mock_http_get.assert_called_once_with(
         "http://mock-url", params={"key": "mock-api-key", "address": "london"}
     )
+    mock_redis_get.assert_called()
+    mock_redis_set.assert_called()
+    assert result == london_expected
 
 
 @pytest.mark.asyncio
+@patch("src.geocoding.redis_client.get")
+@patch("src.geocoding.redis_client.set")
 @patch(
     "src.geocoding.load_url_and_api_key",
     return_value=("http://mock-url", "mock-api-key"),
 )
 @patch("httpx.AsyncClient.get")
-async def test_get_coordinates_empty_results(mock_http_get, mock_load_url_and_api_key):
-    mock_response = MagicMock()
-    mock_response.raise_for_status.return_value = None
-    mock_response.json.return_value = {"results": []}
-    mock_http_get.return_value = mock_response
+async def test_get_coordinates_cache_hit(
+    mock_http_get, mock_load_url_and_api_key, mock_redis_set, mock_redis_get
+):
+    mock_redis_get.return_value = json.dumps(london_fixture)
 
     controller = GeocodingController()
+    result = await controller.get_coordinates([["london"]])
 
+    mock_http_get.assert_not_called()
+    mock_redis_get.assert_called()
+    mock_redis_set.assert_not_called()
+
+    london_expected[0][0]["source"] = "Redis cache"
+    assert result == london_expected
+
+
+@pytest.mark.asyncio
+@patch("src.geocoding.redis_client.get")
+@patch("src.geocoding.redis_client.set")
+@patch(
+    "src.geocoding.load_url_and_api_key",
+    return_value=("http://mock-url", "mock-api-key"),
+)
+@patch("httpx.AsyncClient.get")
+async def test_get_coordinates_empty_results(
+    mock_http_get, mock_load_url_and_api_key, mock_redis_set, mock_redis_get
+):
+    mock_response = httpx.Response(
+        status_code=200,
+        content=json.dumps({"results": []}),
+        request=httpx.Request("GET", "http://mock-url"),
+    )
+    mock_http_get.return_value = mock_response
+    mock_redis_get.return_value = None
+
+    controller = GeocodingController()
     result = await controller.get_coordinates([["The Void"]])
+
+    mock_redis_get.assert_called()
+    mock_redis_set.assert_not_called()
 
     expected = [[{}]]
     assert result == expected
